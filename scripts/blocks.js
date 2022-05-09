4917a4d6c9979417a2324e18ca29cf3b0a380e45
A5TR0spud <supercellstage@gmail.com>
2022-05-08 23:34:17 -0400

    general tweaks

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,28 +1,9 @@
-const gasket = extend(LiquidTurret, "gasket"), {
-	size: 2,
-	health: 1000,
-	range: 110,
-	inaccuracy: 5,
-	shootCone: 50,
-	targetAir: true,
-	maxAmmo: 20,
-	ammoPerShot: 1,
-	recoilAmount: 0,
-	shootSound: none,
-	ammoTypes: {
-		astro-adds-noxious-gas:{
-   	   		type: LiquidBulletType,
-   	   		liquid: astro-adds-noxious-gas,
-      		damage: 0.5,
-      		lifetime: 50,
-      		knockback: 0,
-      		reflectable: false
-		}
-	},
-	requirements: [
-		graphite/75,
-		metaglass/45
-	],
-	category: turret,
-	research: wave
-});
+//const myBlock = extend(Conveyor, "terrible-conveyor", {
+  // various overrides...
+  //size: 3,
+  //health: 200
+  //...
+//});
+//TODO
+//override cultivator
+//override gasket draw
